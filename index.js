059eb8c1dddccb7ca6840373d560d16f200380d1
Bianca Oliveira <bianca.oliv.bn@gmail.com>
2023-12-25 20:07:01 -0300

    repetição completa

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Olá")
+let nome = "Bianca"
+let quantidade = 9000
+
+if(quantidade <= 1000){
+    quantidade = "Ferro"
+    console.log("O Herói de nome" + " " + nome + " " + "está no nível: " + quantidade)
+}else if(quantidade <= 2000){
+    quantidade = "Bronze"
+    console.log("O Herói de nome" + " " + nome + " " + "está no nível: " + quantidade)
+}else if(quantidade <= 5000){
+    quantidade = "Prata"
+    console.log("O Herói de nome" + " " + nome + " " + "está no nível: " + quantidade)
+}else if(quantidade <= 7000){
+    quantidade = "Ouro"
+    console.log("O Herói de nome" + " " + nome + " " + "está no nível: " + quantidade)
+}else if(quantidade <= 8000){
+    quantidade = "Platina"
+    console.log("O Herói de nome" + " " + nome + " " + "está no nível: " + quantidade)
+}else if(quantidade <= 9000){
+    quantidade = "Ascendente"
+    console.log("O Herói de nome" + " " + nome + " " + "está no nível: " + quantidade)
+}else if(quantidade <= 10000){
+    quantidade = "Imortal"
+    console.log("O Herói de nome" + " " + nome + " " + "está no nível: " + quantidade)
+}else if(quantidade >= 10000){
+    quantidade = "Radiante"
+    console.log("O Herói de nome" + " " + nome + " " + "está no nível: " + quantidade)
+}
